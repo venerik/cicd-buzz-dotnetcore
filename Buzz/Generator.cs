2e5da981435bdadc82d5cbbdcb9a00de81d97965
Erik Venema <venerik@dds.nl>
2017-05-05 20:59:56 +0200

    Refactored a bit

--- Buzz/Generator.cs
+++ Buzz/Generator.cs
@@ -1,45 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Buzz
 {
     public class Generator
     {
-        private static string[] _buzz = { "continuous testing", "continuous integration", "continuous deployment", "continuous improvement", "devops" };
-        private static string[] _adjectives = { "complete", "modern", "self-service", "integrated", "end-to-end" };
-        private static string[] _adverbs = { "remarkably", "enormously", "substantially", "significantly", "seriously" };
-        private static string[] _verbs = { "accelerates", "improves", "enhances", "revamps", "boosts" };
+        private static string[] _buzz = { 
+            "continuous testing", 
+            "continuous integration", 
+            "continuous deployment", 
+            "continuous improvement", 
+            "devops" };
+        private static string[] _adjectives = { 
+            "complete", 
+            "modern", 
+            "self-service", 
+            "integrated", 
+            "end-to-end" };
+        private static string[] _adverbs = { 
+            "remarkably", 
+            "enormously", 
+            "substantially", 
+            "significantly", 
+            "seriously" };
+        private static string[] _verbs = { 
+            "accelerates", 
+            "improves", 
+            "enhances", 
+            "revamps", 
+            "boosts" };
 
         private static Random _rnd = new Random();
 
         public string Buzz()
         {
-            var buzzTerms = Sample(_buzz, 2);
-            var phrase = string.Join(" ", Sample(_adjectives), buzzTerms[0], Sample(_adverbs), Sample(_verbs), buzzTerms[1]);
+            string phrase = GeneratePhrase();
             return ToTitleCase(phrase);
         }
 
         public string Sample(IList<string> values)
         {
+            if(values == null || !values.Any())
+            {
+                return string.Empty;
+            }
+
             var index = _rnd.Next(values.Count);
             return values[index];
         }
 
         public IList<string> Sample(IList<string> values, int size)
         {
+            if(values == null || !values.Any() || size < 1)
+            {
+                return new List<string>();
+            }
+
             var samples = new List<string>();
-
-            if(size < 1)
-            {
-                return samples;
-            }
-
-            if(size == values.Count)
-            {
-                return values;
-            }
-
             var source = new List<string>(values);
             while(samples.Count != size)
             {
@@ -49,6 +68,18 @@ namespace Buzz
             }
 
             return samples;
+        }
+
+        private string GeneratePhrase()
+        {
+            var buzzTerms = Sample(_buzz, 2);
+            return string.Join(
+                " ", 
+                Sample(_adjectives), 
+                buzzTerms[0], 
+                Sample(_adverbs), 
+                Sample(_verbs), 
+                buzzTerms[1]);
         }
 
         private static string ToTitleCase(string value)
